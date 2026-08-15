ff7ba0fb9e8220b147b1e426d44f7b5086fc9fec
Ganesh Kumar <itzmeganesh07@gmail.com>
2026-08-15 21:55:42 +0530

    demo of if-else

---
+++ Conditionals/demo.java
@@ -0,0 +1,21 @@
+package Conditionals;
+
+public class demo{
+    public static void main(String[] args){
+        boolean b = true;
+        int i = 10;
+         
+        // Selection statements
+        // Normal if
+        if(i == 10)
+            System.out.println( " I is True ");
+        
+       else 
+       System.out.println( " I is false ");
+       
+    }
+
+    
+}
+// if given only statement in the if-else statenent we can avoid curly braces in both if and else.
+// bytecode --> JVM --> machine code
