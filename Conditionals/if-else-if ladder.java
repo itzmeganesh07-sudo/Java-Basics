fe2bca2287216ddfdae60e5ba44ec741e3f24387
Ganesh Kumar <itzmeganesh07@gmail.com>
2026-08-15 22:14:52 +0530

    if-else-if ladder

---
+++ Conditionals/if-else-if ladder.java
@@ -0,0 +1,23 @@
+package Conditionals;
+
+public class if-else-if ladder {
+    public static void main(String [] args){
+      int i = 6; // initialize 6 in variable i.
+      // conditions
+      if(i==5){
+        System.out.println(" i is 5");
+      }  
+      else if (i == 9) {
+        System.out.println(" i is 9");
+          
+
+      } else if (i==6) {
+        System.out.println("i is 6");
+          
+      } else if (i==2) {
+        System.out.println(" i is 2");
+          
+      }
+    }
+    
+}
