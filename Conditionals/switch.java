f505c065a497869380660ac005e5dbf12e8c118a
Ganesh Kumar <itzmeganesh07@gmail.com>
2026-08-15 22:27:51 +0530

    switch statements

---
+++ Conditionals/switch.java
@@ -0,0 +1,26 @@
+package Conditionals;
+
+public class switch {
+    public static void main(String[] args) {
+        int i = 6;
+     switch(i){
+        case 1:
+            System.out.println(" i is 1");
+            break;
+        case 2:
+            System.out.println("i is 2" );
+            break;
+        case 3:
+            System.out.println(" i i 4");
+            break;
+        case 4:
+             System.out.println(" i is 6");
+             break;   
+     }
+
+    }
+    
+}
+// we don't write break every output will print.
+//eg: i is 1 
+//    i is 2 like that.
