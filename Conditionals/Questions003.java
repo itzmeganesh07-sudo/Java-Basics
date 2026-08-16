a60c30443669a75688ccc485f810dc06eaf0980d
Ganesh Kumar <itzmeganesh07@gmail.com>
2026-08-16 12:59:04 +0530

    Odd Even

---
+++ Conditionals/Questions003.java
@@ -0,0 +1,18 @@
+package Conditionals;
+
+import java.util.Scanner;
+
+public class Questions003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        if(x % 2 == 0){
+            System.out.println("The number is Even");
+        }
+        else{
+            System.out.println("The number is odd");
+        }
+    }
+    
+}
