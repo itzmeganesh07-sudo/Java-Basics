a9dbacf981422882547036455b36d101d246a012
Ganesh Kumar <itzmeganesh07@gmail.com>
2026-08-17 19:05:44 +0530

    Number from 1 to n

---
+++ Loops/Questions002.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Questions002 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+                int a = sc.nextInt();
+
+              for(int i = 1; i<=1; i++){
+                int sum = a*(a+1)/2;
+                 System.out.println(sum);
+        }
+        sc.close();
+    }
+}
+// we can solve it by matematics 
+/*
+for(int i = 1; i<=n ; i++){
+int sum = i*(i+1)/2
+system.out.println(i);
+}
+ */
