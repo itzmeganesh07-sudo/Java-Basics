cb6b867f43c66476ae4d2b60ba3672d6738d8a2f
Ganesh Kumar <itzmeganesh07@gmail.com>
2026-08-17 17:51:52 +0530

    Do while Loop

---
+++ Loops/dowhile.java
@@ -0,0 +1,14 @@
+public class dowhile {
+    public static void main(String[] args) {
+        int i = 1;
+
+        do { 
+            System.out.println("i");
+        } while (i>10);
+    }
+}
+// do while says that the condition executes at list one time.
+// why we need do while :- ex like menu selection in game.
+// play
+// go to new game
+// exit.
