9e2fa5d75ae9d340543979fa136b0a12fc7a525a
Ganesh Kumar <itzmeganesh07@gmail.com>
2026-08-21 17:32:26 +0530

    Reverse an Array

---
+++ array/array5.java
@@ -0,0 +1,10 @@
+package array;
+
+public class array5 {
+    public static void main(String[] args) {
+        int [] arr = {34,5,2,67,95,23,22};
+        for(int i = arr.length -1; i >=0; i--){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
